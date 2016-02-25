a6e86f095abb011ac2719f5d4d1e223e09941256
Daniil <daniil.gentili.dg@gmail.com>
2016-02-25 04:04:43 +0000

    OK madebytravisci

--- debian/video-dl-4.0.46/debian/video-dl/usr/bin/video.sh
+++ debian/video-dl-4.0.46/debian/video-dl/usr/bin/video.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Video download script v4.0.44
+# Video download script v4.0.46
 # Created by Daniil Gentili (http://daniil.it)
 # Video-dl - Video download programs
 #
@@ -28,7 +28,7 @@
 # v3.3.1 Improved the auto update function and player choice
 # v3.3.2 Squashed some other bugs, fixed download of 302 videos on Mac OS X (curl redirection).
 
-echo "Video download script v4.0.44
+echo "Video download script v4.0.46
 Copyright (C) 2016 Daniil Gentili
 This program comes with ABSOLUTELY NO WARRANTY.
 This is free software, and you are welcome to redistribute it under certain conditions; see https://github.com/danog/video-dl/raw/master/LICENSE."
@@ -139,7 +139,7 @@ api() {
 # grep -qE 'http://www.*.rai..*/dl/RaiTV/programmi/media/.*|http://www.*.rai..*/dl/RaiTV/tematiche/.*|http://www.*.rai..*/ dl/.*PublishingBlock-.*|http://www.*.rai..*/dl/replaytv/replaytv.html.*|http://.*.rai.it/.*|http://www.rainews.it/dl/rainews/.*|https://www.*.rai..*/dl/ RaiTV/programmi/media/.*|https://www.*.rai..*/dl/RaiTV/tematiche/.*|https://www.*.rai..*/dl/.*PublishingBlock-.*|https://www.*.rai..*/dl/ replaytv/replaytv.html.*|https://.*.rai.it/.*|https://www.rainews.it/dl/rainews/.*' && ptype=rai
  
  
- echo "$urltype" | grep -qE 'http://www.video.mediaset.it/video/.*|http://www.video.mediaset.it/player/playerIFrame.*|https:// www.video.mediaset.it/video/.*|https://www.video.mediaset.it/player/playerIFrame.*|tgcom24.mediaset.it/video/.*|http://mediaset.it/.*|https:// mediaset.it/.*' && ptype=mediaset
+ echo "$urltype" | grep -qE '.*mediaset.it/video/.*|http://www.video.mediaset.it/player/playerIFrame.*|https://www.video.mediaset.it/video/.*|https://www.video.mediaset.it/player/playerIFrame.*|tgcom24.mediaset.it/video/.*|http://mediaset.it/.*|https://mediaset.it/.*' && ptype=mediaset
  
  echo "$urltype" | grep -qE 'http://.*wittytv.it/.*|https://.*wittytv.it/.*' && ptype=mediaset && witty=y
  
---
+++ debian/video-dl_4.0.46.dsc
@@ -0,0 +1,38 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+Format: 3.0 (native)
+Source: video-dl
+Binary: video-dl
+Architecture: all
+Version: 4.0.46
+Maintainer: Daniil Gentili <daniil.gentili.dg@gmail.com>
+Homepage: http://daniil.it/video-dl
+Standards-Version: 3.9.4
+Build-Depends: debhelper (>= 8.0.0)
+Package-List: 
+ video-dl deb misc optional
+Checksums-Sha1: 
+ 72adcb0dbae79caf988eacb8b80cefa30ecb8a22 19822 video-dl_4.0.46.tar.gz
+Checksums-Sha256: 
+ 57b92fd3585c139cadddff13fc2e05a7c4c1426bc0bed1b4716da5bb8e0afe34 19822 video-dl_4.0.46.tar.gz
+Files: 
+ 569f2bd6b140b3ee389d6561eee07cab 19822 video-dl_4.0.46.tar.gz
+
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1.4.11 (GNU/Linux)
+
+iQIcBAEBAgAGBQJWzn1WAAoJEHK5f9HZZyyTh+gP/jmHMp8eIbrOdAmVNhstSQ22
+/K6UR4cSM0FIYOPP01bd0jXcufSKgF2W+4UvuMjKQw4AFsvYqQYQP2ovXH/bn4xx
+ZjQkMoS8WdNDanBVMI7IWYg4VsZWfkMwIbYOqF15xBq48Vc/tKlU8UBrOqCwIt3J
+00ud/4+pZ3tDn5ylyqCZm/U9me6UuGjMh3ebgoq35bhmTjaAAaFxOFXCWQR7UOim
+JzSKvX2IHbLHlEP7V7zJ5NujC8Y94R0t1DFJ+DnrVoyhzFFi6XYafLBlXAj9aBCK
+m87/uTnSqrpigzBTg9ipd91ydLV3bM+uzmqgU4cw/ad32Iu96Fnw9TbgiDicKV/c
+W310e0WifFD64SLDoJa+3i7ew5rEVzB++9CDRjfu+vhknXN0YwDFSeb+nhU51ba/
+MVkiXryQp7xfvfpdacd0QJWn4UNR6gaYLiEue0TWHMiHgGfWePtHmWv52cStMjwi
+LZ1WOI3iJYa7w2olCP5k/c7kBrZji4PXPtaepGnWhxLD31xL7+m9RqGQUDBCUkm6
+rB60TGmLOlKZ/W3Je80Os0QTrZgk8w6DOP2q9QyIXmyQB6+PCmyt4XpHVJx3kjzn
+1FDBEiLIeIk81gegTZrQ0llIz65d5RVsZ21zZrY/QISByexRrVam2qAYW1JaiLsz
+CDzJIhkbtUSSuWpnbs05
+=EW6f
+-----END PGP SIGNATURE-----
